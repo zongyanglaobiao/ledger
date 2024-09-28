9b223cc5d2ab1a122afde17f742baa00cf255008
xxl <3578144921@qq.com>
2024-09-28 20:29:32 +0800

    feat(首頁): 首页完成

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,31 +1,75 @@
 import {TabBar} from "antd-mobile/2x";
 import {Home, User} from "@icon-park/react";
-import {Button} from "antd-mobile";
-import {getLedger} from "@/http/api/ledger.api.js";
-import {showText} from "@/lib/toolkit/toast.js";
+import './home.css'
+import {Route, Router, Routes} from "react-router-dom";
+
+function UserHome() {
+    return (
+        <div>
+            首页
+        </div>
+    )
+}
+
+function UserSetting() {
+    return (
+        <div>
+            用户设置
+        </div>
+    )
+}
 
 export default () => {
     const tabs = [
         {
             key: 'home',
-            title: '首页',
-            icon: <Home theme="outline" size="30" fill="#9b9b9b" strokeWidth={1}/>,
+            title: (isActive) => {
+                return (
+                    <div className='flex flex-col items-center '>
+                        {
+                            isActive ?
+                                <Home className='leading-0' theme="multi-color" size="25" fill={['#333', '#2F88FF', '#FFF', '#43CCF8']} strokeWidth={1}/>
+                                :
+                                <Home className='leading-0' theme="outline" size="25" fill="#9b9b9b" strokeWidth={1}/>
+                        }
+                        <div className='text-sm'>首页</div>
+                    </div>
+                )
+            },
         },
         {
             key: 'personalCenter',
-            title: <div className='text-sm'>我的</div>,
-            icon: <User theme="outline" size="30" fill="#9b9b9b" strokeWidth={1}/>,
+            title: (isActive) => {
+                return (
+                    <div className='flex flex-col items-center'>
+                        {
+                            isActive ?
+                                <User className='leading-0' theme="multi-color" size="25" fill={['#333', '#2F88FF', '#FFF', '#43CCF8']} strokeWidth={1}/>
+                                :
+                                <User className='leading-0' theme="outline" size="25" fill="#9b9b9b" strokeWidth={1}/>
+                        }
+                        <div className='text-sm'>我的</div>
+                    </div>
+                )
+            }
         },
     ]
 
     return (
         <div className='w-full h-vh layout-center bg-[#fafbfc]'>
-            <TabBar className='w-full h-49px fixed bottom-0 border-t-solid  border-0.5px border-gray'>
+            <Routes>
+                <Route path='/home/user-home' element={<UserHome/>}/>
+                <Route path='/home/user-setting' element={<UserSetting/>}/>
+            </Routes>
+            <TabBar
+                className='w-full h-49px  fixed bottom-3px m-0 p-0 border-t-solid  border-0.5px border-gray'>
                 {tabs.map(item => (
-                    <TabBar.Item key={item.key} icon={item.icon} title={item.title} />
+                    <TabBar.Item key={item.key} title={item.title}/>
                 ))}
             </TabBar>
         </div>
     );
 }
+
+
 
